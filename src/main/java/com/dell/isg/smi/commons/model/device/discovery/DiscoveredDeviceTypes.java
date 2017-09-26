59fb0ee13224f7b488712f6d769d9e6fd6926d99
Prashanth Gowda <Prashanth_L_Gowda@dell.com>
2017-09-26 15:08:02 -0500

    Swagger API documentation changes

--- src/main/java/com/dell/isg/smi/commons/model/device/discovery/DiscoveredDeviceTypes.java
+++ src/main/java/com/dell/isg/smi/commons/model/device/discovery/DiscoveredDeviceTypes.java
@@ -5,16 +5,23 @@ package com.dell.isg.smi.commons.model.device.discovery;
 
 import java.util.ArrayList;
 import java.util.List;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * The Class DiscoveredDeviceTypes.
  */
+
+@ApiModel(value = "DiscoveredDeviceTypes", description = " Discovered devices are grouped by filter types such as SERVER / CHASSIS / SWITCH or STORAGE . ")
 public class DiscoveredDeviceTypes {
 
+	@ApiModelProperty(value = "Device type name .It could be IDRAC6 : IDRAC7 : IDRAC8 : IDRAC9 : CMC : CMC_FX2 : CSERVER : VRTX : VCENTER : FORCE10_S4810 : FORCE10_S5000 : FORCE10_S6000 : FORCE10_S4048 : FORCE10_S55 : FORCE10IOM : FX2_IOM : DELL_IOM_84 : BROCADE : POWERCONNECT : POWERCONNECT_N3000 : POWERCONNECT_N4000 : CISCONEXUS : EQUALLOGIC_NODISCOVER : EM_COMPELLENT : EQUALLOGIC : COMPELLENT ", dataType = "string")
     String deviceName;
 
+	@ApiModelProperty(value = "Number of device typediscovered in the range. ", dataType = "integer")
     int discovered;
 
+	@ApiModelProperty(value = "List of discovered type information.", dataType = "DiscoveredDeviceInfo.class")
     List<DiscoveredDeviceInfo> discoveredDeviceInfoList = new ArrayList<DiscoveredDeviceInfo>();
 
 
